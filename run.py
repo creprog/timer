9f1f5ca6372770b55c574b6219d07138898b5399
creprog <minercre@gmail.com>
2019-12-28 13:27:54 +0300

    Update run.py

--- run.py
+++ run.py
@@ -22,11 +22,6 @@ class StoppableThread(threading.Thread):
 	def stopped(self):
 		return self._stop_event.is_set()
 		
-
-total_seconds = 0
-
-timer_stop = 0
-
 class NKMTime(QtWidgets.QMainWindow):
 	def __init__(self):
 		super(NKMTime, self).__init__()
@@ -37,50 +32,60 @@ class NKMTime(QtWidgets.QMainWindow):
 		self.delta = datetime.timedelta()
 		self.total = datetime.timedelta()
 		self.running = False
-		self.timer = False
+		self.timer_work = False
+		self.timer_rest = False
 		self.ui.pushButton.clicked.connect(self.stopwatch)
 
 	def closeEvent(self, event):
-		self.timer = False
+		self.timer_work = False
+		self.timer_rest = False
 		print('GOING UNDER')
 		event.accept()
 		
 
 	def start_timer(self):
 		timer_start = datetime.datetime.now()
-		self.timer = True
-		print(this.stopped())
-		while self.timer:
-			# time.sleep(1)
-			self.delta=datetime.datetime.now()-timer_start
-			self.ui.label_current_info.setText(str(self.delta).split('.')[0])
+		# print(this.stopped())
+		if self.running:
+			while self.timer_work:
+				# time.sleep(1)
+				self.delta=datetime.datetime.now()-timer_start
+				self.ui.label_current_info.setText(str(self.delta).split('.')[0])
+				time.sleep(1)
+		else:
+			while self.timer_rest:
+				# time.sleep(1)
+				self.delta=datetime.datetime.now()-timer_start
+				self.ui.label_current_info.setText(str(self.delta).split('.')[0])
+				time.sleep(1)
 			# print(self.delta)
 		
 	def stopwatch(self):
 		self.timer = False
 		# self.t.end()
 		if self.running:
-			self.running=False
+			self.ui.label_current.setText('Отдыхаем')
 			self.ui.pushButton.setText('START')
-			# self.time_stop=datetime.datetime.now()
-			# delta = self.time_stop-self.time_start
+			self.running=False
+			self.timer_work = False
+			self.timer_rest = True
 			self.total+=self.delta
-			self.ui.label_total_info.setText(str(self.total))
+			self.ui.label_total_info.setText(str(self.total).split('.')[0])
 			t = StoppableThread(target=self.start_timer)
 			t.start()
 			# self.start_timer()
-			self.ui.label_current.setText('Отдыхаем')
+			
 		else:
-			self.running=True
-			self.ui.pushButton.setText('STOP')
-			self.time_start=datetime.datetime.now()
 			self.ui.label_current.setText('Работаем')
+			self.ui.pushButton.setText('STOP')
+			self.running=True
+			self.timer_work = True
+			self.timer_rest = False
+			self.time_start=datetime.datetime.now()
 			t = StoppableThread(target=self.start_timer)
 			t.start()
 			# self.start_timer()
 
-			
-		
 
 def main():
 	app = QtWidgets.QApplication(sys.argv)
